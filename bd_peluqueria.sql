47567827fdaa13e9b4746de4a7cad194a77ea45e
Lidio Guedez <98057803+abelardog@users.noreply.github.com>
2022-08-11 17:42:04 -0300

    Creando Base de Datos y Tablas

---
+++ bd_peluqueria.sql
@@ -0,0 +1,42 @@
+CREATE SCHEMA `peluqueria_canina` ;
+
+use peluqueria_canina;
+
+CREATE TABLE `dueno` (
+  `DNI` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(50) NULL,
+  `Apellido` VARCHAR(50) NULL,
+  `Telefono` VARCHAR(25) NULL,
+  `Direccion` VARCHAR(80) NULL,
+  PRIMARY KEY (`DNI`));
+
+
+CREATE TABLE `perro` (
+  `ID_Perro` INT NOT NULL AUTO_INCREMENT,
+  `Fecha_nac` DATE NULL,
+  `Sexo` VARCHAR(10) NULL,
+  `DNI_Dueno` INT NULL,
+  PRIMARY KEY (`ID_Perro`),
+  INDEX `DNI_Dueno_idx` (`DNI_Dueno` ASC),
+  CONSTRAINT `DNI_Dueno`
+    FOREIGN KEY (`DNI_Dueno`)
+    REFERENCES `dueno` (`DNI`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  );  
+
+CREATE TABLE `historial` (
+  `ID_Historial` INT NOT NULL AUTO_INCREMENT,
+  `Fecha` DATE NULL,
+  `Perro` INT NULL,
+  `Descripcion` VARCHAR(100) NULL,
+  `Monto` DECIMAL NULL,
+  PRIMARY KEY (`ID_Historial`),
+  INDEX `Perro_idx` (`Perro` ASC),
+  CONSTRAINT `Perro`
+    FOREIGN KEY (`Perro`)
+    REFERENCES `perro` (`ID_Perro`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  );
+
