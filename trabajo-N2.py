c7155890a16dd3e34bf70d6c08e17e9bf374d8fc
Lidio Guedez <98057803+abelardog@users.noreply.github.com>
2022-08-11 10:50:52 -0300

    Creando ejercicio 2 para evaluacion de python
    2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
    
               23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”
    
     que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección)

---
+++ trabajo-N2.py
@@ -0,0 +1,10 @@
+Dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+indice = 0
+longitud = len(Dueno2)
+while indice < longitud:
+    valor = Dueno2[indice]
+    if indice != 0 and indice != 2:
+         print(valor)
+    indice += 1    
+
